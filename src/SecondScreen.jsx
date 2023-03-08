c10cc60b7ca1527d4732210b5b6f93324f532257
Octavian <sirghi1979@mail.ru>
2023-03-08 15:59:48 +0300

    init

--- src/SecondScreen.jsx
+++ src/SecondScreen.jsx
@@ -8,10 +8,10 @@ function SecondScreen() {
     <div className="min-h-[100vh] flex items-center justify-center">
       <div className="text-xl sm:text-2xl lg:text-4xl textHiddenContainer">
         <span className="text-cyan-500">{"<"} </span>
-        Поздравляю тебя {name.replace(/"/gi, "")} с 8 марта{" "}
+        Поздравляю тебя, {name.replace(/"/gi, "")}, с 8 марта{" "}
         <GiCottonFlower className="inline-block text-red-500" />
         <span className="textHidden">
-          . Если ты оказался парнем, то увы сегодня не твой день {":)"}
+          . Если ты оказался парнем, то, увы, сегодня не твой день {":)"}
         </span>
         <span className="text-cyan-500"> {"/>"}</span>
       </div>
